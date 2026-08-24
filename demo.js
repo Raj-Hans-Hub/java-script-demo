c5009ce66482a1c7b9195d4d22a7b5d1f8df6af8
Raj Hans <mahadevheights9999@gmail.com>
2026-08-24 13:19:46 +0530

    push demo if

--- demo.js
+++ demo.js
@@ -1,126 +1,63 @@
+console.log("1. if Statement");
 
 
-const  customer_name  = "Amit Kumar"; //String
-const  movie_name  = "Shaitan"; //String
-const   ticket_price = 1025; //Number
-let number_of_tickets  = 98; //Number
+let battery = 19;
 
-// console.log(customer_name ,movie_name , ticket_price ,number_of_tickets );
+if (battery < 20) {
 
-
-
-const student_name = "mohit sharma"; //String
-const age = 25;  //Number
-const course = "Mern Stack"; //String
-const city = "jaipur"; //String
-let  admission_status = false; //Boolean
-
-// console.log(student_name ,age , course ,city,admission_status );
+   console.log("Low Battery");
+}
 
 
 
-const passenger_name = "Bablu Khokhar";  //String
-const source = "Sikar"; //String
-const destination = "Jaipur"; //String
-const ticketprice = 250; //Number
-const seat_number  = 12; //Number
-let booking_status = "pending"; //String
+let temperature = 41;
 
+if (temperature > 40) {
 
-// console.log(passenger_name ,source , destination ,ticketprice ,seat_number,booking_status );
-
-
-const employee_name = "Raj Kumar"; //String
-const employee_ID = 165; //Number
-const department = "MRD"; //String
-const salary = 15000; //Number
-const joining_year  = 2020; //Number
-let active_status = true; //Boolean
-
-// console.log(employee_name ,employee_ID , department ,salary ,joining_year ,active_status );
-
-const studentname = "Rohit bajoliya";  //String
-const email = "rohitbbc@gmail.com"; //String
-const exam_name = "CRPF SI"; //String
-const registration_number = 126548462 ; //Number 
-const exam_fee  = 526 ; //Number
-let registration_status = "confimed" ; //String
-
-// console.log(studentname ,email , exam_name ,registration_number, exam_fee , registration_status  );
-
-
-const campaign_name = "Booster Race" ;  //String
-const client_name = "Ajaj Ali " ; //String
-const platform = "Twitter" ; //String
-const budget = 69000 ; //Number
-const duration  = 30 ; //Number
-let campaign_status = "Running" ; //String
+   console.log("Very Hot");
+}
 
 
 
-const product_name = "OPPO v16 promax" ; //String
-const brand = "OPPO" ; //String
-const model = 92023 ; //Number
-const price = 21999 ; //Number
-const mfg_date = "06/08/2026" ; //String
-let quantity = 56 ; //Number
-var payment_status = "confirmed" ; //String
-var delivery_status = "pending" ; //String
-const payment_mode = "CASH" ; //String
-const customername = "Rahul" ; //String
-const Phone_no = 9352456252 ; //Number
-const Address = "banshiwala market sikar " ; //String
+let seats = 1;
+
+if (seats > 0) {
+
+   console.log("Seats Available");
+
+}
 
 
 
- console.log("product_name" ,product_name);
- console.log("bran" ,brand);
- console.log("mode" ,model);
- console.log("pric" ,price);
- console.log("mfg_dat" ,mfg_date);
- console.log("quantity",quantity);
- console.log("payment_status",payment_status);
- console.log("delivery_status ",delivery_status);
- console.log("payment_mode",payment_mode);
- console.log("customernam" ,customername);
- console.log("Phone_no",Phone_no);
- console.log("Address",Address);
+let stock = 4;
 
+if (stock < 5) {
 
-"Amit" //String
-25 //Number
-true //Boolean
-null //null
-undefined //undefined
-"Sikar" //String
-49999  //Number
-false //Boolean
+   console.log("Stock is Low");
+
+}
 
 
 
 
+let isRaining = true;
 
-var stud_name = "Rohul";
-var stud_name = "mohit";
+if (isRaining == true) {
 
-// function scoped
-// when need to change the value of vaiables
-//  var is the old way to create variables
-// it can be re-declared
+   console.log("Take an Umbrella");
 
-let student = "Rohul";
-student = "mohit";
-
-
-// Block scoped
-// Cannot re-declare
-// let is used to declare variables that can change.
-// Use let when value may change.
+}
 
 
 
-const stu_name = "Rohul";
 
-// Block scoped
-// const is used for variables that should not change.
-// Cannot reassign?
+let balance = 5;
+
+if (balance > 0) {
+
+   console.log("Balance Available");
+
+}
+
+
+
