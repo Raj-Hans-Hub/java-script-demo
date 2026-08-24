84d1dd8835906baa6bde05b6b6b5252f9eedcc42
Raj Hans <mahadevheights9999@gmail.com>
2026-08-24 10:29:43 +0530

    push ternary file

---
+++ ternary.js
@@ -0,0 +1,28 @@
+
+
+console.log("5. Ternary Operator");
+
+
+let isDarkMode = 1;
+
+isDarkMode == 0 ? console.log("Dark Mode"):console.log("Light Mode");
+
+
+let isVerified = false ? console.log("Verified"):console.log("Not verified");
+
+
+let hasCoupon = true ? console.log("Coupon Applie"):console.log("Coupon No Applie");
+
+
+
+let canDownload = true ? console.log("Download Available"):console.log("Download Not Available");
+
+let username = "sinfode";
+
+let password = 1234;
+
+ let isLoggedIn = username == "sinfode" &&  password == 1234 ? true:false;
+ 
+isLoggedIn == true ? console.log("Welcome"):console.log("Please Login");
+
+
