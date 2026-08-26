357648902eaa9593c2e2e0cb213c6ab2d9548228
Raj Hans <mahadevheights9999@gmail.com>
2026-08-26 11:27:35 +0530

    push seitch condition file

---
+++ switch.js
@@ -0,0 +1,147 @@
+
+console.log("4. switch Statement");
+
+
+let  language ="py";
+
+switch (language) {
+    case "js":
+        console.log("JAVA SCRIPT");        
+        break;
+
+    case "py":
+        console.log("Python");        
+        break;
+
+    case "jv":
+        console.log("JAVA ");        
+        break;
+
+    default:
+         console.log("No Access");
+}
+
+
+
+
+let  platform = "Facebook";
+
+switch (platform) {
+
+    case platform:
+        console.log( platform);
+                
+        break;
+
+}
+
+
+
+let fileType ="png";
+
+switch (fileType) {
+    case "jpg":
+        console.log(`fileType: ${fileType} `);        
+        break;
+
+    case "jpeg":
+        console.log(`fileType: ${fileType} `);
+        break;
+
+    case "mp3":
+        console.log(`fileType: ${fileType} `);
+        break;
+
+    case "mp4":
+        console.log(`fileType: ${fileType} `);
+        break;
+
+    case "png":
+        console.log(`fileType: ${fileType} `);
+        break;
+
+    case "doex":
+        console.log(`fileType: ${fileType} `);
+        break;
+
+    default:
+         console.log("No Access");
+
+        
+}
+
+
+
+
+let month = 6;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+
+    case 2:
+        console.log("February");
+        
+        break;
+
+    case 3:
+        console.log("March");
+        
+        break;
+
+    case 4:
+        console.log("April");
+        
+        break;
+
+    case 5:
+        console.log("May");
+        
+        break;
+
+    case 6:
+        console.log("June");
+        
+        break;
+
+    case 7:
+        console.log("July");
+        
+        break;
+    case 8:
+
+        console.log("august");
+        
+        break;
+
+    case 9:
+        console.log("September");
+        
+        break;
+    case 10:
+
+        console.log("October");
+        
+        break;
+    case 11:
+
+        console.log("November");
+        
+        break;
+    case 12:
+
+        console.log("December");
+        
+        break;
+
+    default:
+       console.log("No Access");
+        
+        break;
+}
+
+
+
+
